e49307690932a18427bcda217bb76b24d546b0a5
Jose Aleixo Araujo Porpino Filho <aleixoporpino@gmail.com>
2019-03-16 16:26:31 -0400

    Added restaurant

--- banco.sql
+++ banco.sql
@@ -215,6 +215,25 @@ CREATE TABLE `agendacontato`.`User`
   PRIMARY KEY (`id`)
 );
 
+-- auto-generated definition
+create table Restaurant
+(
+  id                int auto_increment
+    primary key,
+  name              varchar(50)          not null,
+  description       varchar(500)         not null,
+  brief_description varchar(50)          null,
+  address           varchar(200)         not null,
+  tags              varchar(200)         not null,
+  phone             varchar(20)          not null,
+  favorite          tinyint(1) default 0 not null,
+  stars             int        default 0 not null
+);
+
+
+INSERT INTO agendacontato.Restaurant (id, name, description, brief_description, address, tags, phone, favorite, stars) VALUES (2, 'Red Lobster', 'ticaratica', 'Fresh Fish - Live Lobster', '10 Cardwell ave
+Unit 2', 'lobster, shrimp, seafood', '6475617767', 0, 3);
+INSERT INTO agendacontato.Restaurant (id, name, description, brief_description, address, tags, phone, favorite, stars) VALUES (3, 'Chipotle', 'Mexican Grill', 'Mexican Grill', '300 Borough Dr UNIT 17/18, Scarborough, ON M1P 4P5', 'mexican, taco, burrito, bowl', '(647) 348-4750', 1, 3);
 
 SET
 SQL_MODE
